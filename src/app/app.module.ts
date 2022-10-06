089aa448218e25b6e52df455a4283832e5f25939
Alejandro Solorio <jale59@hotmail.com>
2022-10-05 20:45:05 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () =>
+      import("./tiers/tiers.module").then((m) => m.TiersModule),
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/tiers/tiers.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+
+import { ListComponent } from "./_components/list/list.component";
+import { FormComponent } from "./_components/form/form.component";
+import { MainComponent } from "./_components/main/main.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent,
+    children: [
+      { path: "list", component: ListComponent },
+      { path: "form", component: FormComponent },
+      { path: "form/:tierid", component: FormComponent },
+      { path: "**", component: ListComponent },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [ListComponent, FormComponent, MainComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class TiersModule {}
